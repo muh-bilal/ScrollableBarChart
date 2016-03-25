018c2a888d4e9aa5fd49e72069a9bf48d7b62280
M.Bilal <muh.bilal@live.com>
2016-03-25 16:53:31 +0500

    Initial commit

---
+++ ScrollableBarChart/Constants/Constants.h
@@ -0,0 +1,18 @@
+//
+//  Constants.h
+//  ScrollableBarChart
+//
+//  Created by M.Bilal on 25/03/2016.
+//  Copyright © 2016 M.Bilal. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#define IS_IPAD UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
+#define IS_IPHONE UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone
+
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
+#endif /* Constants_h */
