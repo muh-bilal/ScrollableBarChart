f5a8f992c2720d7431e0c523b8ccb978efbc56b5
M Bilal <Bilal@bilals-pc.local>
2016-03-25 15:33:40 +0500

    Initial Commit

---
+++ ScrollableBarChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollableBarChart
+//
+//  Created by M.Bilal on 25/03/2016.
+//  Copyright © 2016 M.Bilal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
